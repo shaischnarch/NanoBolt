5cd75336e60ae09407f428693f7577aa219b9170
shaischnarch <shai.schnarch@gmail.com>
2020-06-13 18:02:09 +0300

    changed parameters

--- Helper_directory/Main_helper.py
+++ Helper_directory/Main_helper.py
@@ -3,12 +3,12 @@ from Excution.All_executions import *
 
 ## function used to set robot to the zero position
 def zero_position():
-    #execute_movement(0, (90, 135, 90))
-    #execute_movement(1, (90, 80, 60))
-    #execute_movement(2, (90, 45, 90))
-    #execute_movement(3, (90, 100, 120))
-    execute_movement(0, (90, 90, 90))
-    execute_movement(1, (90, 90, 90))
-    execute_movement(2, (90, 90, 90))
-    execute_movement(3, (90, 90, 90))
+    execute_movement(0, (90, 135, 90))
+    execute_movement(1, (90, 80, 60))
+    execute_movement(2, (90, 45, 90))
+    execute_movement(3, (90, 100, 120))
+    #execute_movement(0, (90, 90, 90))
+    #execute_movement(1, (90, 90, 90))
+    #execute_movement(2, (90, 90, 90))
+    #execute_movement(3, (90, 90, 90))
     return [(-25, -150, -40), (25, -150, 40), (25, -150, -40), (-25, -150, 40)]
