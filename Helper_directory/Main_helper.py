ed7da42033ae7e24f02b71a5950cc3d5e9eebb25
shaischnarch <shai.schnarch@gmail.com>
2020-09-14 13:39:33 +0300

    changed zero position to receive values from user

--- Helper_directory/Main_helper.py
+++ Helper_directory/Main_helper.py
@@ -1,12 +1,28 @@
 from Excution.All_executions import *
-
+from Calculations.All_calculations import legIK, servo_angles
+#from Helper_directory import Settings
 
 ## function used to set robot to the zero position
 def zero_position():
-    execute_movement(0, (90, 135, 90))
-    execute_movement(1, (90, 80, 60))
-    execute_movement(2, (90, 45, 90))
-    execute_movement(3, (90, 120, 120))
+    not_finished = True
+    while not_finished:
+        for i in range(4):
+            a = input("enter offset for leg: " + str(i))
+            a = tuple(int(x) for x in a.split(","))
+            (offset1, offset2, offset3) = a
+            (theta1, theta2, theta3) = legIK(-25 + offset1, -150 + offset2, 0 + offset3)
+            angles_servo = servo_angles([(theta1, theta2, theta3)], i)
+            execute_movement(i, angles_servo[0])
+
+        b = input("are you finished? ")
+        if b == 'yes':
+            not_finished = False
+
+
+    #execute_movement(0, (90, 135, 90))
+    #execute_movement(1, (90, 80, 60))
+    #xecute_movement(2, (90, 45, 90))
+    #execute_movement(3, (90, 120, 120))
     #execute_movement(0, (90, 90, 90))
     #execute_movement(1, (90, 90, 90))
     #execute_movement(2, (90, 90, 90))
