0629a9e1c65959aaa0c3be73e50c5576a0826136
shaischnarch <shai.schnarch@gmail.com>
2020-10-04 16:33:03 +0300

    try to fix two leg mode with smaller touching legs increments

--- Main.py
+++ Main.py
@@ -10,14 +10,103 @@ import time
 import board
 import busio
 import adafruit_bno055
+import math
 
 
 ######### *temp* for stand mode ###### changed in shai
-def stand_pre_execution(sensor):
+sensor_act = 1 # the value under which the robot is considered flat
+def stand_pre_execution(sensor, sensor_offset):
 	(euler1, euler2, euler3) = sensor.euler
 	print((euler1, euler2, euler3))
+	offsetsX= [0,0,0,0]
+	offsetsY = [0,0,0,0]
+	offsetsZ = [0,0,0,0]
+
+	if (abs(euler2) > sensor_act):
+		offsetsY[0] -= np.sign(euler2) * (1 + int(math.fabs(euler2)/5))
+		offsetsY[1] -= np.sign(euler2) * (1 + int(math.fabs(euler2)/5))
+		offsetsY[2] += np.sign(euler2) * (1 + int(math.fabs(euler2)/5))
+		offsetsY[3] += np.sign(euler2) * (1 + int(math.fabs(euler2)/5))
+
+		#offsetsZ[0] += np.sign(euler2) * (1 + int(math.fabs(euler2) / 5))
+		#offsetsZ[1] += np.sign(euler2) * (1 + int(math.fabs(euler2) / 5))
+		#offsetsZ[2] += np.sign(euler2) * (1 + int(math.fabs(euler2) / 5))
+		#offsetsZ[3] += np.sign(euler2) * (1 + int(math.fabs(euler2) / 5))
+
+	if (abs(euler3) > sensor_act):
+		offsetsY[0] += np.sign(euler3) * (1 + int(math.fabs(euler3)/5))
+		offsetsY[1] -= np.sign(euler3) * (1 + int(math.fabs(euler3)/5))
+		offsetsY[2] -= np.sign(euler3) * (1 + int(math.fabs(euler3)/5))
+		offsetsY[3] += np.sign(euler3) * (1 + int(math.fabs(euler3)/5))
+
+		#offsetsX[0] += np.sign(euler3) * (1 + int(math.fabs(euler3) / 5))
+		#offsetsX[1] -= np.sign(euler3) * (1 + int(math.fabs(euler3) / 5))
+		#offsetsX[2] -= np.sign(euler3) * (1 + int(math.fabs(euler3) / 5))
+		#offsetsX[3] += np.sign(euler3) * (1 + int(math.fabs(euler3) / 5))
+
+	for j in range(4):
+		lst = list(sensor_offset[j])
+		lst[0] += offsetsX[j]
+		lst[1] += offsetsY[j]
+		lst[2] += offsetsZ[j]
+		sensor_offset[j] = tuple(lst)
+	print(sensor_offset)
+	return sensor_offset
+
+###########################################################
+######### *temp* for 2-leg mode ###### changed in shai
+sensor_act = 1 # the value under which the robot is considered flat
+ratio = 1/3 # the ration between x and z, (z/x)
+air_multiplier = 3
+def two_leg_pre_execution(sensor, sensor_offset, two_leg_lean):
+	(euler1, euler2, euler3) = sensor.euler
+	print((euler1, euler2, euler3))
+	offsetsX= [0,0,0,0]
+	offsetsY = [0,0,0,0]
+	offsetsZ = [0,0,0,0]
+
+	if (two_leg_lean):
+		if (abs(euler2) > sensor_act and abs(euler3) > sensor_act):
+			offsetsZ[0] += np.sign(euler2) * (1 + int(math.fabs(euler2) / 5)) * ratio
+			offsetsZ[2] += np.sign(euler2) * (1 + int(math.fabs(euler2) / 5)) * ratio
+
+			offsetsX[0] += np.sign(euler3) * (1 + int(math.fabs(euler2) / 5))
+			offsetsX[2] -= np.sign(euler3) * (1 + int(math.fabs(euler2) / 5))
+
+			offsetsZ[1] -= np.sign(euler2) * (1 + int(math.fabs(euler2) / 5)) * air_multiplier * ratio
+			offsetsZ[3] -= np.sign(euler2) * (1 + int(math.fabs(euler2) / 5)) * air_multiplier * ratio
+
+			offsetsX[1] += np.sign(euler3) * (1 + int(math.fabs(euler2) / 5)) * air_multiplier
+			offsetsX[3] -= np.sign(euler3) * (1 + int(math.fabs(euler2) / 5)) * air_multiplier
+	else:
+		if (abs(euler2) > sensor_act and abs(euler3) > sensor_act):
+			offsetsZ[1] += np.sign(euler2) * (1 + int(math.fabs(euler2) / 5)) * ratio
+			offsetsZ[3] += np.sign(euler2) * (1 + int(math.fabs(euler2) / 5)) * ratio
+
+			offsetsX[1] -= np.sign(euler3) * (1 + int(math.fabs(euler2) / 5))
+			offsetsX[3] += np.sign(euler3) * (1 + int(math.fabs(euler2) / 5))
+
+			offsetsZ[0] -= np.sign(euler2) * (1 + int(math.fabs(euler2) / 5)) * air_multiplier * ratio
+			offsetsZ[2] -= np.sign(euler2) * (1 + int(math.fabs(euler2) / 5)) * air_multiplier * ratio
+
+			offsetsX[0] -= np.sign(euler3) * (1 + int(math.fabs(euler2) / 5)) * air_multiplier
+			offsetsX[2] += np.sign(euler3) * (1 + int(math.fabs(euler2) / 5)) * air_multiplier
 
 
+	for j in range(4):
+		lst = list(sensor_offset[j])
+		lst[0] += offsetsX[j]
+		lst[1] += offsetsY[j]
+		lst[2] += offsetsZ[j]
+		sensor_offset[j] = tuple(lst)
+	print(sensor_offset)
+	return sensor_offset
+
+
+
+
+
+###########################################################
 # Led Pin Definitions
 led = digitalio.DigitalInOut(board.D18)
 led.direction = digitalio.Direction.OUTPUT
@@ -40,6 +129,13 @@ last_substep_num = 0
 ####current_substep_num = [0, 0, 0, 0]
 ####last_substep_num = [0, 0, 0, 0]
 is_stand = True
+is_2_leg = False
+two_leg_lean = True ## True means legs 0 and 2 are touching the ground, False means legs 1 and 3 are touching the ground
+two_leg_path = [[], [], [], []]
+two_leg_offset = 40
+two_leg_substeps = 80
+two_leg_counter = 0
+
 
 current_leg_locations = zero_position()
 sleep(1)  # gives enough time to get to zero position
@@ -56,24 +152,80 @@ while True:
 				#turn led on
 				led.value = True
 				print('Controller Connected')
-
+				sensor_offset = [(0,0,0), (0,0,0), (0,0,0), (0,0,0)]
 				# main loop
 				while (ds4.connected):
+					(left_cx, left_cy, right_cx, right_cy, buttons_pressed) = controller_call(ds4)
+					print(type(buttons_pressed))
+					print(buttons_pressed)
+
+
+					################### TWO LEG SWITCH #########################
+					if 'circle' in ds4.presses:
+						print("hello yall")
+						is_stand = False
+						is_2_leg = True
+						sensor_offset = [(0,0,0), (0,0,0), (0,0,0), (0,0,0)]
+						two_leg_path[0] = calculate_points(current_leg_locations[0], current_leg_locations[0], 0, two_leg_substeps)
+						two_leg_path[1] = calculate_points(current_leg_locations[1], current_leg_locations[1], 0, two_leg_substeps)
+						two_leg_path[2] = calculate_points(current_leg_locations[2], current_leg_locations[2], 0, two_leg_substeps)
+						two_leg_path[3] = calculate_points(current_leg_locations[3], current_leg_locations[3], 0, two_leg_substeps)
+						if (two_leg_lean):
+							(x,y,z) = current_leg_locations[1]
+							two_leg_path[1] = calculate_points((x,y,z),(x,y+two_leg_offset,z),0,two_leg_substeps)
+							(x, y, z) = current_leg_locations[3]
+							two_leg_path[3] = calculate_points((x, y, z), (x, y + two_leg_offset, z), 0, two_leg_substeps)
+						else:
+							(x, y, z) = current_leg_locations[0]
+							two_leg_path[0] = calculate_points((x, y, z), (x, y + two_leg_offset, z), 0, two_leg_substeps)
+							(x, y, z) = current_leg_locations[2]
+							two_leg_path[2] = calculate_points((x, y, z), (x, y + two_leg_offset, z), 0, two_leg_substeps)
+
+					################### END OF TWO LEG SWITCH #########################
+
+
 
 					###### STANDING MODE ########
 					if is_stand:
 						default_x = Settings.default_x
 						default_y = Settings.default_y
 						default_z = Settings.default_z
-						sensor_offset = stand_pre_execution(sensor)
+						sensor_offset = stand_pre_execution(sensor, sensor_offset)
 						for i in range(4):
 							(offset1, offset2, offset3) = Settings.legs_offset[i]
+							if i>2: 
+								offset3= offset3-10
 							(sensor_offset1, sensor_offset2, sensor_offset3) = sensor_offset[i]
 							(theta1, theta2, theta3) = legIK(default_x + offset1 + sensor_offset1, default_y + offset2 + sensor_offset2, default_z + offset3 + sensor_offset3)
+							current_leg_locations[i] = (default_x + sensor_offset1, default_y + sensor_offset2, default_z + sensor_offset3)
 							angles_servo = servo_angles([(theta1, theta2, theta3)], i)
 							execute_movement(i, angles_servo[0])
+						sleep(Settings.max_delay)
 						continue
 					###### END OF STANDING MODE ########
+
+					###### 2 LEG MODE ########
+					if is_2_leg:
+						sensor_offset = two_leg_pre_execution(sensor, sensor_offset, two_leg_lean)
+						for i in range(4):
+							(offset1, offset2, offset3) = Settings.legs_offset[i]
+							(x,y,z) = two_leg_path[i][two_leg_counter]
+							if i > 2:
+								offset3 = offset3 - 10
+							(sensor_offset1, sensor_offset2, sensor_offset3) = sensor_offset[i]
+							(theta1, theta2, theta3) = legIK(x + int(offset1) + sensor_offset1, y + int(offset2) + sensor_offset2, z + int(offset3) + sensor_offset3)
+							angles_servo = servo_angles([(theta1, theta2, theta3)], i)
+							execute_movement(i, angles_servo[0])
+						sleep(Settings.max_delay)
+						if (two_leg_counter < two_leg_substeps):
+								two_leg_counter += 1
+						continue
+					##### END OF 2 LEG MODE ########
+
+
+
+
+
 
 
 
