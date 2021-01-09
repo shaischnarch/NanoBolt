4765177a01ab74fd82b45ac7ff4098d5d6b70762
shaischnarch <shai.schnarch@gmail.com>
2021-01-09 15:08:34 +0200

    added sensor to 2 legs walk

--- Modes_directory/Walking_2_legs_Class.py
+++ Modes_directory/Walking_2_legs_Class.py
@@ -1,5 +1,5 @@
 from Helper_directory import Settings
-from Helper_directory.Main_helper import move_to_stand, move_to_position
+from Helper_directory.Main_helper import move_to_stand, move_to_position, read_sensor
 from Calculations.All_calculations import calculate_points, servo_angles, legIK
 import numpy as np
 import math
@@ -38,20 +38,68 @@ class Walking_2_legs(Mode):
         # change for testing
         # define max and min substep delay:
         self.substep_delay_jumps = 0.005
-        self.max_substep_delay = Settings.max_delay+ self.substep_delay_jumps*8
+        self.max_substep_delay = Settings.max_delay + self.substep_delay_jumps*8
         self.min_substep_delay = Settings.min_delay
 
+        ## sensors variables
+        self.sensor_active = 0
+        self.num_of_substeps_sensor_checker = 1
+        self.sensor_angle_unit = 5  # it means that every time,we add another correction units for every additional 5 degrees diffrence from wanted angle
+        self.sensor_z_axis_offset_units = 1
+        self.sensor_x_axis_offset_units = 1
+        self.sensor_y_axis_offset_units = 1
+
+        # define the wanted sensors angles and what is the maximum distance allowed from those angles
+        self.wanted_front_angle = 2
+        self.front_angle_allowed_diff = 1
+        self.wanted_side_angle = 0
+        self.side_angle_allowed_diff = 1
+
+    #OVERRIDE
     def plan_movement(self, left_cx, left_cy, right_cx, right_cy, buttons_pressed):
 
         if self.diag == 0:
             self.diag = 1
             self.end_points = Settings.second_diag_default[:]
-            self.heights = [0,self.height,0,self.height] # change
+            self.heights = [0, self.height, 0, self.height]
+
+            self.sensor_offset[1] = (0, 0, 0)  # change
+            self.sensor_offset[3] = (0, 0, 0)  # change
+            self.controller_offset[1] = (0, 0, 0)  # change
+            self.controller_offset[3] = (0, 0, 0)  # change
 
         else:
             self.diag = 0
             self.end_points = Settings.first_diag_default[:]
-            self.heights = [self.height, 0, self.height, 0] # change
+            self.heights = [self.height, 0, self.height, 0]  # change
+
+            self.sensor_offset[0] = (0, 0, 0)  # change
+            self.sensor_offset[2] = (0, 0, 0)  # change
+            self.controller_offset[0] = (0, 0, 0)  # change
+            self.controller_offset[2] = (0, 0, 0)  # change
+
+
+    # @OVERRIDE - add the ability to calculate the removal of offsets for the correct legs
+    # Main function for calculating all the substeps locations along the a step.
+    # Therefore it is called only after plan_movement and it finishes setting up the new step.
+    # @Receives: nothing
+    # @Resets: is_finished_step = false, current_substep = 0
+    # @Returns nothing
+    def calculate_points(self):
+        if self.diag == 1:
+            self.points[0] = calculate_points(self.semi_ideal_current_pos[0], self.end_points[0], self.heights[0], self.num_of_substeps)
+            self.points[2] = calculate_points(self.semi_ideal_current_pos[2], self.end_points[2], self.heights[2], self.num_of_substeps)
+            self.points[1] = calculate_points(self.current_legs_location[1], self.end_points[1], self.heights[1], self.num_of_substeps)
+            self.points[3] = calculate_points(self.current_legs_location[3], self.end_points[3], self.heights[3], self.num_of_substeps)
+        else:
+            self.points[1] = calculate_points(self.semi_ideal_current_pos[1], self.end_points[1], self.heights[1], self.num_of_substeps)
+            self.points[3] = calculate_points(self.semi_ideal_current_pos[3], self.end_points[3], self.heights[3], self.num_of_substeps)
+            self.points[0] = calculate_points(self.current_legs_location[0], self.end_points[0], self.heights[0], self.num_of_substeps)
+            self.points[2] = calculate_points(self.current_legs_location[2], self.end_points[2], self.heights[2], self.num_of_substeps)
+        # self.current_substep = 0
+        self.current_substep = 1  # EXPERIMENTAL, might need to be lowered back down to 0
+        self.is_finished_step = False
+
 
 
 
@@ -61,6 +109,13 @@ class Walking_2_legs(Mode):
     def controller_input(self, left_cx, left_cy, right_cx, right_cy, buttons_pressed):
 
     # change (all of that)
+        # Activate / DeActivate Sensor
+        if 'dup' in buttons_pressed:
+            self.sensor_active = not self.sensor_active
+            # Note:  The current sensor offset is still in the system, we are not removing it to not cause a sudden leg position jump
+            #        It will automatically be removed after one or two steps
+
+        # Pause / UnPause movement
         if 'r1' in buttons_pressed:
             self.pause_movement = not self.pause_movement
             print("paused: " + str(self.pause_movement))
@@ -105,7 +160,92 @@ class Walking_2_legs(Mode):
             print("Substep delay is already in it's min value: " + str(self.min_substep_delay))
 
 
-    # for now use the version form Mode. todo: write a version to take the sensor into consideration
-    # def prep_substep(self, sensor):
-    #     pass
+    # This method is called upon before executing the next substep.
+    # When this method is called, the location of the next semi-ideal substep is already available in self.points[leg_num][self.current_substep]
+    # Therefore this method uses the sensor to make realtime minor changes to the next substep - transfer to physical/dynamic realm
+    # It than calculates the actual values for the servos to execute the next substep
+    # @updates: current_legs_location with the real time location, and semi_ideal_current_pos with the semi ideal position
+    # todo: might be a cause of error, debug later
+    def prep_substep(self, sensor):
+        if self.current_substep % self.num_of_substeps_sensor_checker == 0 and self.sensor_active == 1:
+            self.__Walking_2_legs_sensor_helper(sensor)
+        self.__sum_offsets()
+        for leg_num in range(4):
+            (point_x, point_y, point_z) = self.points[leg_num][self.current_substep]  # The calculated target location
+            (offset_x, offset_y, offset_z) = self.offsets[leg_num]
+            x = point_x + offset_x
+            y = point_y + offset_y
+            z = point_z + offset_z
+            #print("leg {} final point value: {}".format(leg_num, (x,y,z)))
+            try:
+                (theta1, theta2, theta3) = legIK(x, y, z)
+                self.angles_servo[leg_num] = servo_angles([(theta1, theta2, theta3)], leg_num)
+                self.current_legs_location[leg_num] = (x, y, z)  # real location
+                self.semi_ideal_current_pos[leg_num] = (point_x, point_y, point_z)  # semi-ideal location
+            except:
+                print('ERROR: Tried to move to impossible position')
+                self.stop_movement = True
 
+
+
+    def __Walking_2_legs_sensor_helper(self, sensor):
+        (euler1, euler2, euler3) = read_sensor(sensor)
+        print((euler1, euler2, euler3))
+        offsetsX = [0, 0, 0, 0]
+        offsetsY = [0, 0, 0, 0]
+        offsetsZ = [0, 0, 0, 0]
+
+        synced_front_angle = euler2 - self.wanted_front_angle
+        synced_side_angle = euler3 - self.wanted_side_angle
+        if self.diag == 0:  ## legs 0 and 2 are in the air, while legs 1 and 3 on the ground
+            if (abs(synced_front_angle) > self.front_angle_allowed_diff):
+                ##offsetsY[0] -= np.sign(synced_front_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                #offsetsY[1] -= np.sign(synced_front_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                ##offsetsY[2] += np.sign(synced_front_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                #offsetsY[3] += np.sign(synced_front_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+
+                ##offsetsZ[0] += np.sign(synced_front_angle) * (self.sensor_z_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                offsetsZ[1] += np.sign(synced_front_angle) * (self.sensor_z_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                ##offsetsZ[2] += np.sign(synced_front_angle) * (self.sensor_z_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                offsetsZ[3] += np.sign(synced_front_angle) * (self.sensor_z_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+
+            if (abs(synced_side_angle) > self.side_angle_allowed_diff):
+                ##offsetsY[0] += np.sign(synced_side_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                #offsetsY[1] -= np.sign(synced_side_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                ##offsetsY[2] -= np.sign(synced_side_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                #offsetsY[3] += np.sign(synced_side_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+
+                ##offsetsX[0] += np.sign(synced_side_angle) * (self.sensor_x_axis_offset_units+int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                offsetsX[1] -= np.sign(synced_side_angle) * (self.sensor_x_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                ##offsetsX[2] -= np.sign(synced_side_angle) * (self.sensor_x_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                offsetsX[3] += np.sign(synced_side_angle) * (self.sensor_x_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+
+        elif self.diag == 1: ## legs 1 and 3 are in the air, legs 0 and 2 on the ground.
+            if (abs(synced_front_angle) > self.front_angle_allowed_diff):
+                #offsetsY[0] -= np.sign(synced_front_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                ## offsetsY[1] -= np.sign(synced_front_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                #offsetsY[2] += np.sign(synced_front_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                ## offsetsY[3] += np.sign(synced_front_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+
+                offsetsZ[0] += np.sign(synced_front_angle) * (self.sensor_z_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                ##offsetsZ[1] += np.sign(synced_front_angle) * (self.sensor_z_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                offsetsZ[2] += np.sign(synced_front_angle) * (self.sensor_z_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+                ##offsetsZ[3] += np.sign(synced_front_angle) * (self.sensor_z_axis_offset_units + int(math.fabs(synced_front_angle) / self.sensor_angle_unit))
+
+            if (abs(synced_side_angle) > self.side_angle_allowed_diff):
+                #offsetsY[0] += np.sign(synced_side_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                ## offsetsY[1] -= np.sign(synced_side_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                #offsetsY[2] -= np.sign(synced_side_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                ## offsetsY[3] += np.sign(synced_side_angle) * (self.sensor_y_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+
+                offsetsX[0] += np.sign(synced_side_angle) * (self.sensor_x_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                ##offsetsX[1] -= np.sign(synced_side_angle) * (self.sensor_x_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                offsetsX[2] -= np.sign(synced_side_angle) * (self.sensor_x_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+                ##offsetsX[3] += np.sign(synced_side_angle) * (self.sensor_x_axis_offset_units + int(math.fabs(synced_side_angle) / self.sensor_angle_unit))
+
+        for j in range(4):
+            lst = list(self.sensor_offset[j])
+            lst[0] += offsetsX[j]
+            lst[1] += offsetsY[j]
+            lst[2] += offsetsZ[j]
+            self.sensor_offset[j] = tuple(lst)
